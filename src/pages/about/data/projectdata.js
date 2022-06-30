baab921a334930b54d9094814ab8b09fc5dde8dc
kamalrossi <rossikamal@gmail.com>
2022-06-30 17:06:14 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,56 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "HealthAnalytics";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "HealthAnalytics is  a responsive dashboard which helps clinicians to visualize an manage hospital acitivies. Major modules involve doctor, patient, appointment, department, billing, inventory, etc. Doctors are able to visualize patient admission, revenue earning, department activites and event appointment notifications through interactive visual graphics."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "ChartJS",
+    tag3: "D3.JS",
+    tag4: "MedTech",
+    
+  }
+];
+const projecttime = [
+  {
+    title: "Requirement Analysis",
+    from: "Oct 21",
+    to: "June 22"
+  },
+  {
+    title: "Design and Implementation",
+    from: "Oct 21",
+    to: "June 22"
+  }
+];
+
+
+const projectrole = [
+  {
+    title: "UX conceptualization",
+    desc:
+      "conceptualization of data visualization techniques required in hospital decision making."
+  },
+  {
+    title: "Responsive Graph/Web Implementation",
+    desc:
+      "Develop visualization API and software modules required in digital medical platform."
+  },
+  {
+    title: "Data Integration:",
+    desc:
+      "Integrate standalone graphics API in admin dashboard of hospital management."
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
